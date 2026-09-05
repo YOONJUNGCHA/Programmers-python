ce8c9b7d34c73a1292821a6fe34fb94dd892fd0a
YOONJUNG <chayj0421@gmail.com>
2026-09-05 11:49:40 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,11 @@
+
+
+def solution(my_string, overwrite_string, s):
+    a = my_string[0:s]
+    b = overwrite_string
+    c = my_string[(s + len(overwrite_string)): ]
+    
+    
+    
+    return a + b + c
+
