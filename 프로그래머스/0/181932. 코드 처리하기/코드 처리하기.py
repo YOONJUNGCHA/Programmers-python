39eb7c3914351d3cd978e144043e855c31c4d0fb
YOONJUNG <chayj0421@gmail.com>
2026-09-07 17:32:31 +0900

    [level 0] Title: 코드 처리하기, Time: 7.45 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,17 @@
+def solution(code):
+    ret = ''
+    mode = 0
+
+    for i in range(len(code)):
+        if code[i] == '1':
+            mode = 1 - mode
+        else:
+            if mode == 0 and i % 2 == 0:
+                ret += code[i]
+            elif mode == 1 and i % 2 == 1:
+                ret += code[i]
+
+    if ret == '':
+        return 'EMPTY'
+
+    return ret
